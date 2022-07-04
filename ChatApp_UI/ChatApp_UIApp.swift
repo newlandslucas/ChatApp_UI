2102f3490e6280c4299490a49ea293e5c221a78c
newlandslucas <lucasnewlands.m@gmail.com>
2022-07-04 16:09:55 -0300

    Initial Commit

---
+++ ChatApp_UI/ChatApp_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp_UIApp.swift
+//  ChatApp_UI
+//
+//  Created by Lucas Machado (Estudos) on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
