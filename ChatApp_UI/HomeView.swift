4bb74ac89aa5dabf13b6d68d5a66f74c7d9c897e
newlandslucas <lucasnewlands.m@gmail.com>
2022-07-04 16:16:38 -0300

    feature: project setup

---
+++ ChatApp_UI/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  ChatApp_UI
+//
+//  Created by Lucas Machado (Estudos) on 04/07/22.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            
+            HStack {
+                
+                Button {
+                    
+                } label: {
+                    <#code#>
+                }
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
